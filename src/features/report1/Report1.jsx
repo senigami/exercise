e56f4abc2381a057ca199e892efc107255f54864
Steven Dunn <sd@stevenandnicole.com>
2020-08-12 01:15:45 -0400

    Initial Fetching of Data
    will use fetch for Report 1 and axios for Report 2

--- src/features/report1/Report1.jsx
+++ src/features/report1/Report1.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 export class Report1 extends Component {
+  state = {
+    data: null,
+  };
+
+  async componentDidMount() {
+    fetch('https://www.defense.gov/data.json')
+      .then((response) => response.json())
+      .then((data) => this.setState({ data: data }));
+  }
+
   render() {
+    const ListData = () => {
+      console.log(this.state.data);
+      return null;
+    };
+
     return (
       <div>
         <h1>Report 1</h1>
+        {this.state.data ? <ListData /> : <h1>loading...</h1>}
       </div>
     );
   }
